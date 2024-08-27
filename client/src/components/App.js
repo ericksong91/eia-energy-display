cd0f85d1f4cd59daea4ce7583831d7d8db4c9666
Eric Song <ericksong91@gmail.com>
2024-08-27 15:25:07 -0400

    Testing Chart.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,41 @@
-import logo from '../logo.svg';
+import Chart from 'chart.js/auto'
+import { CategoryScale } from 'chart.js/auto';
+import { useState } from 'react';
+import { Data } from "../utils/Data";
 import '../css/App.css';
 
+Chart.register(CategoryScale);
+
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+
+      //Inserting an object 
+
+      {
+        label: "Users Gained",
+        data: Data.map((data) => data.userGain),
+        // Background color is an array of values for determining color
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+
+  console.log(Data)
+  console.log(chartData)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
     </div>
   );
 }
