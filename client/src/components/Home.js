f0ee09156fae1eaf8a67d1f1bc77808ba19f38e8
Eric Song <ericksong91@gmail.com>
2024-09-04 13:00:29 -0400

    Spitballing ideas for data display

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="Home">
+        <p>Spitballing ideas for chart options</p>
+        <h1>Emissions per State</h1>
+        <h1>Energy Generation per State</h1>
+        <h1>Total Emissions for USA</h1>
+        <h1>Total Energy Generation for USA</h1>
+        <h1>Nuclear Generators in USA</h1>
+        <h2>Locations, Energy Generation?, Which Gen reactor?</h2>
+    </div>
+  )
+}
+
+export default Home
