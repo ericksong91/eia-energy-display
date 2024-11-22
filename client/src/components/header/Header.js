8fd35d2943604100a03b7adb639a3d77e8ad90fd
Eric Song <ericksong91@gmail.com>
2024-11-22 11:40:56 -0500

    Fixed header display issues and separated out light dark toggle to its own component

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import logo from '../../images/logo.png'
 import Navbar from './Navbar'
+import LightDarkToggle from './LightDarkToggle';
 
 function Header() {
     return (
-        <header className='header flex items-center w-full bg-blue-900 bg-opacity-30'>
-            <img src={logo} alt="EIA Energy Data Logo has a lightbulb with the website name" className='logo w-32 object-cover rounded-lg border-2 mx-4 my-2' />
-            <Navbar />
+        <header className='header items-center bg-blue-900 bg-opacity-30'>
+            <div className='container mx-auto'>
+                <div className='flex items-center'>
+                    <img src={logo} alt="EIA Energy Data Logo has a lightbulb with the website name" className='logo w-32 object-cover rounded-lg border-2 mx-4 my-2' />
+                    <Navbar />
+                    <LightDarkToggle />
+                </div>
+            </div>
+
+
         </header>
     )
 }
--- client/src/components/header/Navbar.js
+++ client/src/components/header/Navbar.js
@@ -10,40 +10,18 @@ function Navbar() {
     <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
   </svg>;
 
-  const moonIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-  </svg>;
-
-  const sunIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-  </svg>;
-
   const buttonsList = [homeIcon, aboutIcon].map((x) => {
-    return <button className='bg-slate-200 rounded p-2 mx-3'>
+    return <li><button className='bg-slate-200 rounded p-2 mx-3'>
       {x}
-    </button>
+    </button></li>
   });
 
   return (
-    <section className='navbar container flex w-full'>
-      <div className='buttons flex'>
-        {buttonsList}
-      </div>
-
-      <div className='lightdark-mode container flex justify-end'>
-        <label class="inline-flex items-center cursor-pointer">
-          <span class="ms-3 mr-3">
-            {sunIcon}
-          </span>
-          <input type="checkbox" value="" class="sr-only peer" />
-          <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
-            dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
-            peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
-            after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        </label>
-
-      </div>
-    </section>
+    <nav className='navbar grow'>
+      <ul className='flex'>
+          {buttonsList}
+      </ul>
+    </nav>
   )
 };
 
