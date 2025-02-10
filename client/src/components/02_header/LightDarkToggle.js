702c3f0a6ffcaa266dbd47be5a0f4a3503c7bab3
Eric Song <ericksong91@gmail.com>
2025-02-10 14:42:31 -0500

    Added an icon provider for all used SVG icons

--- client/src/components/02_header/LightDarkToggle.js
+++ client/src/components/02_header/LightDarkToggle.js
@@ -1,20 +1,20 @@
 import { useContext } from "react";
-import SunIcon from '../../images/icons/SunIcon';
-import MoonIcon from '../../images/icons/MoonIcon';
+import { IconContext } from "../00_context/IconContext";
 import { DarkModeContext } from "../00_context/DarkModeContext";
 
 
 function LightDarkToggle() {
     const { darkMode, setDarkMode } = useContext(DarkModeContext);
+    const { sunIcon, moonIcon } = useContext(IconContext);
 
     return (
-            <label className={`flex items-center align-middle cursor-pointer`}>
-                <span className="sm:mr-3">
-                    {darkMode ? <MoonIcon /> : <SunIcon />}
-                </span>
+        <label className={`flex items-center align-middle cursor-pointer`}>
+            <span className="sm:mr-3">
+                {darkMode ? moonIcon : sunIcon}
+            </span>
 
-                <input type="checkbox" value="" className="sr-only peer" onClick={() => setDarkMode(!darkMode)} />
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
+            <input type="checkbox" value="" className="sr-only peer" onClick={() => setDarkMode(!darkMode)} />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
                     dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
                     peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] 
                     after:bg-white after:border-gray-300 after:border after:rounded-full 
@@ -22,8 +22,8 @@ function LightDarkToggle() {
                     
                     hidden sm:block
                     ">
-                </div>
-            </label>
+            </div>
+        </label>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import { DarkModeProvider } from './components/00_context/DarkModeContext';
+import { IconProvider } from './components/00_context/IconContext';
 import './css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <DarkModeProvider>
-      {/* <Router> */}
-      <App />
-      {/* </Router> */}
+      <IconProvider>
+        <App />
+      </IconProvider>
     </DarkModeProvider>
   </StrictMode>
 );
