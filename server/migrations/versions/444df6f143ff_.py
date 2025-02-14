bcb8fe24a061f62e04c9daceb2a68fda551d094e
Eric Song <ericksong91@gmail.com>
2025-02-14 10:15:45 -0500

    check redeploy

---
+++ server/migrations/versions/444df6f143ff_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 444df6f143ff
+Revises: 4c9be9f55862
+Create Date: 2025-02-14 10:14:15.944775
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '444df6f143ff'
+down_revision = '4c9be9f55862'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
