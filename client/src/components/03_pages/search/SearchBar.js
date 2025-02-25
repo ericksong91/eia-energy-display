515338cf7e901b3567fce2b41d41a59558e44624
Eric Song <ericksong91@gmail.com>
2025-02-24 19:43:52 -0500

    Fixed combobox styling to conform to current styling

--- client/src/components/03_pages/search/SearchBar.js
+++ client/src/components/03_pages/search/SearchBar.js
@@ -3,7 +3,7 @@ import { useCombobox } from 'downshift';
 import { IconContext } from '../../00_context/IconContext';
 
 function SearchBar({ onUpdateGraphs }) {
-  const { magnifyingGlass, states } = useContext(IconContext);
+  const { upArrow, downArrow, magnifyingGlass, states } = useContext(IconContext);
   const [inputItems, setInputItems] = useState(states);
 
   function handleStatesFilter(value) {
@@ -15,43 +15,52 @@ function SearchBar({ onUpdateGraphs }) {
     return result;
   }; // filters search terms
 
-  const { isOpen, getToggleButtonProps, getLabelProps, getMenuProps, getInputProps, highlightedIndex, getItemProps, selectedItem, selectItem, } =
+  const { isOpen, getToggleButtonProps, getLabelProps, getMenuProps, getInputProps, highlightedIndex, getItemProps, selectedItem, } =
     useCombobox(
       {
         items: inputItems,
         onInputValueChange: ({ inputValue }) => {
           setInputItems(handleStatesFilter(inputValue));
         },
-        onSelectedItemChange:
-          ({ selectedItem, inputValue }) => {
-            onUpdateGraphs(selectedItem, inputValue);
-          },
+        onSelectedItemChange: ({ selectedItem }) => {
+          onUpdateGraphs(selectedItem);
+        },
       });
 
   return (
-    <div className='search-bar-combobox'>
-      <label {...getLabelProps()}>
-        Choose an element:
+    <div className='search-bar-combobox w-full mx-auto'>
+      <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white" {...getLabelProps()}>
+        Search for States or Click to Select States
       </label>
-      <div className='input-and-select-menu'>
-        <input {...getInputProps()} data-testid="combobox-input"/>
-        <button aria-label="toggle menu" data-testid="combobox-toggle-button" {...getToggleButtonProps()}>
-          {isOpen ? <>up arrow</> : <>down arrow</>}
-        </button>
-        <button aria-label="toggle menu" data-testid="clear-button" onClick={() => selectItem(null)}>
-          Clear Field
-        </button>
-      </div>
 
-      <ul className='select-options' {...getMenuProps()}>
-        {isOpen &&
-          inputItems.map((item, index) => (
-            <li className={`${item}-option`} key={`${item}${index}`} {...getItemProps({item,index,})}>
-              {item}
-            </li>
-          ))}
-      </ul>
-    </div>
+      <div className="relative">
+          <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+            {magnifyingGlass}
+          </div>
+
+          <input {...getInputProps()}
+            className='combobox-search block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg 
+            bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+            dark:focus:border-blue-500'
+            placeholder="Search for States or Click to Select States..."
+          />
+
+          <button {...getToggleButtonProps()} className='toggle-select-options text-light-text absolute end-2.5 bottom-2.5 bg-light-accent hover:bg-opacity-40 focus:ring-2 focus:outline-none 
+            focus:ring-light-text font-medium rounded-lg text-sm px-4 py-2 dark:bg-dark-text dark:hover:bg-opacity-70 dark:focus:ring-dark-accent'>
+            {isOpen ? <>&#8593;</> : <>&#8595;</>}
+          </button>
+        </div>
+
+        <ul className={`select-options hide-scrollbar w-full max-h-64 overflow-y-scroll flex-col ${isOpen ? 'bg-white rounded-lg shadow-lg p-3 z-50 dark:bg-dark-background' : ''}`} {...getMenuProps()}>
+          {isOpen &&
+            inputItems.map((item, index) => (
+              <li key={`${item}${index}`} {...getItemProps({ item, index, })}
+                className={`${item}-option ${selectedItem === item ? 'font-bold' : 'font-normal'} ${highlightedIndex === index ? 'bg-blue-100' : ''} cursor-pointer`}>
+                {item}
+              </li>
+            ))}
+        </ul>
+      </div>
   )
 };
 
